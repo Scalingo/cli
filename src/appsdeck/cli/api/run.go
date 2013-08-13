16ca1e8c88ad8a8400a351e5da17ebeda48efb80
Soulou <leo@unbekandt.eu>
2013-08-13 18:28:00 +0200

    fix api endpoint to run job

--- src/appsdeck/cli/api/run.go
+++ src/appsdeck/cli/api/run.go
@@ -2,13 +2,16 @@ package api
 
 import (
 	"net/http"
+	"strings"
 )
 
 func Run(app string, command []string) (*http.Response, error) {
 	req := map[string]interface{}{
 		"method":   "POST",
-		"endpoint": "/apps/" + app + "/run",
-		"host": "10.1.0.2:10006",
+		"endpoint": "/api/apps/" + app + "/run",
+		"params": map[string]interface{}{
+			"command": strings.Join(command, " "),
+		},
 	}
 	return Do(req)
 }
