48c8929ab19ceb97518fa48a8a54e036f2c9bf29
Soulou <leo@unbekandt.eu>
2013-08-13 18:30:44 +0200

    fix command run declaration

--- src/appsdeck/cli/cmd/run.go
+++ src/appsdeck/cli/cmd/run.go
@@ -3,15 +3,14 @@ package cmd
 import (
 	"appsdeck/cli/appdetect"
 	"appsdeck/cli/apps"
-	"fmt"
 	"github.com/Appsdeck/cli"
 )
 
 var (
 	RunCommand = cli.Command{
-		Name:        "run",
-		ShortName:   "r",
-		Usage:       "Run any command for your app",
+		Name:      "run",
+		ShortName: "r",
+		Usage:     "Run any command for your app",
 		Description: `Run command in current app context, your application
    environment will be loaded and you can execute any task (example
    'rake' or any database-related task)`,
@@ -21,8 +20,6 @@ var (
 				cli.ShowCommandHelp(c, "run")
 			} else if err := apps.Run(currentApp, c.Args()); err != nil {
 				errorQuit(err)
-			} else {
-				fmt.Println(c.Args())
 			}
 		},
 	}
