2a83696c99f38043a704c034de6ea1367fdbcb66
Soulou <leo@unbekandt.eu>
2013-08-13 18:31:18 +0200

    Run custom command routine, http hijack with appsdeck-run

--- src/appsdeck/cli/apps/run.go
+++ src/appsdeck/cli/apps/run.go
@@ -2,8 +2,15 @@ package apps
 
 import (
 	"appsdeck/cli/api"
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"syscall"
 )
 
 func Run(app string, command []string) error {
@@ -11,13 +18,98 @@ func Run(app string, command []string) error {
 	if err != nil {
 		return err
 	}
-	defer res.Body.Close()
+	runStruct := make(map[string]interface{})
+	ReadJson(res.Body, &runStruct)
 
-	buffer, err := ioutil.ReadAll(res.Body)
+	if _, ok := runStruct["attach"]; !ok {
+		return fmt.Errorf("Unexpected answer from server")
+	}
+
+	res, socket, err := connectToRunServer(runStruct["attach"].(string))
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != 200 {
+		return fmt.Errorf("Unauthorized")
+	}
+
+	sttyArgs := []string{"stty", "-echo", "raw"}
+	fd := []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd()}
+	_, err = syscall.ForkExec("/bin/stty", sttyArgs, &syscall.ProcAttr{Dir: "", Files: fd})
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(string(buffer))
+	running := true
+	stdinChan := readerToChan(os.Stdin)
+	socketChan := readerToChan(socket)
+
+	for running {
+		select {
+		case input, ok := <-stdinChan:
+			if ok {
+				fmt.Fprint(socket, input)
+			} else {
+				running = false
+			}
+		case data, ok := <-socketChan:
+			if ok {
+				fmt.Print(data)
+			} else {
+				os.Stdin.Close()
+				running = false
+			}
+		}
+	}
+	socket.Close()
+
+	sttyArgs = []string{"stty", "echo", "cooked"}
+	fd = []uintptr{os.Stdout.Fd(), os.Stderr.Fd()}
+	syscall.ForkExec("/bin/stty", sttyArgs, &syscall.ProcAttr{Dir: "", Files: fd})
 	return nil
+}
+
+func connectToRunServer(rawUrl string) (*http.Response, net.Conn, error) {
+	req, err := http.NewRequest("POST", rawUrl, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	api.AddAuthToken(req)
+
+	url, err := url.Parse(rawUrl)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	dial, err := net.Dial("tcp", url.Host)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	conn := httputil.NewClientConn(dial, nil)
+
+	res, err := conn.Do(req)
+	if err != httputil.ErrPersistEOF && err != nil {
+		return nil, nil, err
+	}
+	connection, _ := conn.Hijack()
+	return res, connection, nil
+}
+
+func readerToChan(in io.Reader) chan string {
+	c := make(chan string, 10)
+	reader := bufio.NewReader(in)
+	go func() {
+		for {
+			r, n, err := reader.ReadRune()
+			if err != nil {
+				close(c)
+				break
+			}
+			if n > 0 {
+				c <- string(r)
+			}
+		}
+	}()
+	return c
 }
