bf6710e61cd8adde4c455357c7907e749a6b315e
Soulou <leo@unbekandt.eu>
2014-12-11 23:51:24 +0100

    Improve indentation helper

--- io/indent.go
+++ io/indent.go
@@ -9,8 +9,12 @@ func Indent(data string, level int) string {
 	indent := genIndent(level)
 	lines := strings.Split(data, "\n")
 	buffer := new(bytes.Buffer)
-	for _, line := range lines {
-		buffer.WriteString(indent + line)
+	for i, line := range lines {
+		if i == len(lines)-1 {
+			buffer.WriteString(indent + line)
+		} else {
+			buffer.WriteString(indent + line + "\n")
+		}
 	}
 	return buffer.String()
 }
