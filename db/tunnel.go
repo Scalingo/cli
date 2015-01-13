5949cd6355f55f693ae8364f5138dcbad6924d19
Soulou <leo@unbekandt.eu>
2015-01-13 14:09:04 +0100

    Handle AES-127-CBC encrypted SSH RSA keys

--- db/tunnel.go
+++ db/tunnel.go
@@ -1,16 +1,25 @@
 package db
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/pem"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net"
 	"net/url"
+	"strings"
 	"sync"
+
 	"golang.org/x/crypto/ssh"
 
 	"github.com/Scalingo/cli/api"
 	"github.com/Scalingo/cli/config"
+	"github.com/Scalingo/cli/term"
 	"gopkg.in/errgo.v1"
 )
 
@@ -94,12 +103,69 @@ func sshPrivateKey(path string) (ssh.Signer, error) {
 		return nil, errgo.Mask(err)
 	}
 
+	// We parse the private key on our own first so that we can
+	// show a nicer error if the private key has a password.
+	block, _ := pem.Decode(privateKey)
+	if block == nil {
+		return nil, fmt.Errorf(
+			"Failed to read key '%s': no key found", path)
+	}
+	if block.Headers["Proc-Type"] == "4,ENCRYPTED" {
+		splitCipher := strings.Split(block.Headers["DEK-Info"], ",")
+		cipherType, iv := splitCipher[0], strings.TrimSpace(splitCipher[1])
+		switch cipherType {
+		case "AES-128-CBC":
+			decryptedKey, err := decryptAES128Key(block.Bytes, iv)
+			if err != nil {
+				return nil, errgo.Mask(err)
+			}
+			decryptedBlock := &pem.Block{}
+			decryptedBlock.Type = block.Type
+			decryptedBlock.Bytes = decryptedKey
+			privateKey = pem.EncodeToMemory(decryptedBlock)
+		default:
+			return nil, fmt.Errorf(
+				"Failed to read key '%s': password protected keys with '%s' are\n"+
+					"not supported. Please decrypt the key prior to use.", path, cipherType)
+		}
+	}
+
 	privateKeySigner, err := ssh.ParsePrivateKey(privateKey)
 	if err != nil {
 		return nil, errgo.Mask(err)
 	}
 
 	return privateKeySigner, nil
+}
+
+func decryptAES128Key(payload []byte, iv string) ([]byte, error) {
+	password, err := term.Password("Encrypted SSH Key, password: ")
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+
+	ivBytes, err := hex.DecodeString(iv)
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+
+	key := append([]byte(password), ivBytes[0:8]...)
+	keyHash := md5.New()
+	_, err = keyHash.Write(key)
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+	keyHashed := keyHash.Sum(nil)
+
+	decryptedPayload := make([]byte, len(payload))
+	block, err := aes.NewCipher(keyHashed)
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+	decrypter := cipher.NewCBCDecrypter(block, ivBytes)
+	decrypter.CryptBlocks(decryptedPayload, payload)
+	decryptedPayload = bytes.TrimRight(decryptedPayload, "\n")
+	return decryptedPayload, nil
 }
 
 func handleConnToTunnel(sshClient *ssh.Client, dbUrl *url.URL, sock net.Conn, errs chan error) {
