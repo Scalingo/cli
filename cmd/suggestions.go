0f4ce425caccb9ccf19865c7bea7a644285c3f4b
bertrand jung <bertrand.jung@epitech.eu>
2015-10-20 14:59:55 +0200

    Reduced number of char to match to 2

--- cmd/suggestions.go
+++ cmd/suggestions.go
@@ -11,9 +11,9 @@ func ShowSuggestions(c *cli.Context) {
 	var suggestions []string
 	cmdName := c.Args().First()
 
-	startRange := 3
-	if 3 >= len(cmdName) {
-		startRange = len(cmdName) % 4
+	startRange := 2
+	if 2 >= len(cmdName) {
+		startRange = len(cmdName) % 3
 	}
 	endRange := len(cmdName) - startRange
 
@@ -24,33 +24,6 @@ func ShowSuggestions(c *cli.Context) {
 			} else if strings.HasSuffix(cmd.Name, cmdName[endRange:]) {
 				suggestions = append(suggestions, cmd.Name)
 			}
-		}
-	}
-
-	if len(c.Args()) > 1 {
-		alikeCmd := ""
-		matches := 0
-		tmpMatches := 0
-		for _, cmd := range c.App.Commands {
-			for _, a := range c.Args() {
-				for _, f := range cmd.Flags {
-					for _, s := range strings.Split(f.String(), ", ") {
-						if s == a {
-							tmpMatches += 1
-						}
-					}
-				}
-			}
-			if tmpMatches > matches {
-				alikeCmd = cmd.Name
-				matches = tmpMatches
-			}
-			tmpMatches = 0
-		}
-
-		if alikeCmd != "" {
-			fullCmd := "scalingo " + alikeCmd + " " + strings.Join(c.Args()[1:], " ")
-			suggestions = append(suggestions, fullCmd)
 		}
 	}
 
