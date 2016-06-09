fa66dc1c69089c6c4c9fb4a910caa842ebabb408
Jonathan Hurter <jonathan@hurter.fr>
2016-06-09 09:58:33 +0200

    Add buildpack flag to create

--- cmd/create.go
+++ cmd/create.go
@@ -12,13 +12,16 @@ var (
 		ShortName:   "c",
 		Category:    "Global",
 		Description: "Create a new app:\n   Example:\n     'scalingo create mynewapp'\n     'scalingo create mynewapp --remote \"staging\"'",
-		Flags:       []cli.Flag{cli.StringFlag{Name: "remote", Value: "scalingo", Usage: "Remote to add to your current git repository", EnvVar: ""}},
+		Flags:       []cli.Flag{
+			cli.StringFlag{Name: "remote", Value: "scalingo", Usage: "Remote to add to your current git repository", EnvVar: ""},
+			cli.StringFlag{Name: "buildpack", Value: "", Usage: "URL to a custom buildpack that Scalingo should use to build your application", EnvVar:""},
+		},
 		Usage:       "Create a new app",
 		Action: func(c *cli.Context) {
 			if len(c.Args()) != 1 {
 				cli.ShowCommandHelp(c, "create")
 			} else {
-				err := apps.Create(c.Args()[0], c.String("remote"))
+				err := apps.Create(c.Args()[0], c.String("remote"), c.String("buildpack"))
 				if err != nil {
 					errorQuit(err)
 				}
