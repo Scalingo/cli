aae4de4c2341bec5f5ad86cbc0251b16d231812f
Mathias Bragagia <mathias.bragagia@epitech.eu>
2017-06-12 14:02:02 +0200

    typo

--- update/check.go
+++ update/check.go
@@ -42,8 +42,8 @@ func Check() error {
 	<-gotLastVersion
 
 	if gotAnError {
-		fmt.Println("An error append when connecting on scalingo server.")
-		return errgo.New("Timeout when connecting on scalingo server.")
+		fmt.Println("Update checker: connection error")
+		return errgo.New("Update checker: connection error")
 	}
 	if version == lastVersion {
 		return nil
