addf1376f41c101633e7518f54c049d494f80d72
MrYawe <yannis.weishaupt@gmail.com>
2017-07-07 17:39:22 +0200

    Update go-scalingo

---
+++ vendor/github.com/Scalingo/go-scalingo/logs-archives.go
@@ -0,0 +1,82 @@
+package scalingo
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"strconv"
+
+	errgo "gopkg.in/errgo.v1"
+)
+
+type LogsArchiveItem struct {
+	Url  string `json:"url"`
+	From string `json:"from"`
+	To   string `json:"to"`
+	Size int64  `json:"size"`
+}
+
+type LogsArchivesResponse struct {
+	NextCursor string            `json:"next_cursor"`
+	HasMore    bool              `json:"has_more"`
+	Archives   []LogsArchiveItem `json:"archives"`
+}
+
+func (c *Client) LogsArchivesByCursor(app string, cursor string) (*LogsArchivesResponse, error) {
+	req := &APIRequest{
+		Client:   c,
+		Endpoint: "/apps/" + app + "/logs_archives",
+		Params: map[string]string{
+			"cursor": cursor,
+		},
+	}
+
+	res, err := req.Do()
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+
+	var logsRes = LogsArchivesResponse{}
+	err = json.Unmarshal(body, &logsRes)
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+
+	return &logsRes, nil
+}
+
+func (c *Client) LogsArchives(app string, page int) (*LogsArchivesResponse, error) {
+	if page < 1 {
+		return nil, errgo.New("Page must be greater than 0.")
+	}
+
+	req := &APIRequest{
+		Client:   c,
+		Endpoint: "/apps/" + app + "/logs_archives",
+		Params: map[string]string{
+			"page": strconv.FormatInt(int64(page), 10),
+		},
+	}
+
+	res, err := req.Do()
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+
+	var logsRes = LogsArchivesResponse{}
+	err = json.Unmarshal(body, &logsRes)
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+
+	return &logsRes, nil
+}
