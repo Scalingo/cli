05b8976b4aef9b8e515321b594dcc4d2b63e78b3
Soulou <leo@unbekandt.eu>
2018-01-26 18:53:48 +0100

    Implement rename command Fixes #330

---
+++ apps/rename.go
@@ -0,0 +1,41 @@
+package apps
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/Scalingo/cli/config"
+	"github.com/Scalingo/cli/io"
+	"gopkg.in/errgo.v1"
+)
+
+func Rename(appName string, newName string) error {
+	var validationName string
+
+	c := config.ScalingoClient()
+	_, err := c.AppsShow(appName)
+	if err != nil {
+		return errgo.Mask(err, errgo.Any)
+	}
+
+	fmt.Printf("/!\\ You're going to rename '%s' to '%s'\nTo confirm type the name of the application: ", appName, newName)
+	validationName, err = bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		return errgo.Mask(err, errgo.Any)
+	}
+	validationName = strings.Trim(validationName, "\n")
+
+	if validationName != appName {
+		return errgo.Newf("'%s' is not '%s', aborting…\n", validationName, appName)
+	}
+
+	_, err = c.AppsRename(appName, newName)
+	if err != nil {
+		return errgo.Notef(err, "fail to name app")
+	}
+
+	io.Status("App " + appName + " has been renamed to " + newName)
+	return nil
+}
