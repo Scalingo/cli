d993181654ab79960c34229820c99f4201d8ef18
Étienne Michon <etienne@scalingo.com>
2019-04-16 16:51:49 +0200

    Display Let's Encrypt certificate status

--- domains/list.go
+++ domains/list.go
@@ -5,9 +5,18 @@ import (
 	"os"
 
 	"github.com/Scalingo/cli/config"
+	scalingo "github.com/Scalingo/go-scalingo"
 	"github.com/olekukonko/tablewriter"
 	"gopkg.in/errgo.v1"
 )
+
+var letsencryptStatusString = map[string]string{
+	string(scalingo.LetsEncryptStatusPendingDNS):  "Pending DNS",
+	string(scalingo.LetsEncryptStatusNew):         "Creating",
+	string(scalingo.LetsEncryptStatusCreated):     "In use",
+	string(scalingo.LetsEncryptStatusDNSRequired): "DNS required",
+	string(scalingo.LetsEncryptStatusError):       "Error",
+}
 
 func List(app string) error {
 	c := config.ScalingoClient()
@@ -17,7 +26,7 @@ func List(app string) error {
 	}
 
 	t := tablewriter.NewWriter(os.Stdout)
-	t.SetHeader([]string{"Domain", "SSL"})
+	t.SetHeader([]string{"Domain", "TLS/SSL"})
 	hasCanonical := false
 
 	for _, domain := range domains {
@@ -30,7 +39,15 @@ func List(app string) error {
 		if !domain.SSL {
 			row = append(row, "-")
 		} else {
-			row = append(row, fmt.Sprintf("Valid until %v", domain.Validity))
+			var tls string
+			if domain.LetsEncrypt {
+				letsencryptStatus, ok := letsencryptStatusString[string(domain.LetsEncryptStatus)]
+				if !ok {
+					letsencryptStatus = string(domain.LetsEncryptStatus)
+				}
+				tls = fmt.Sprintf("(LE %s)", letsencryptStatus)
+			}
+			row = append(row, fmt.Sprintf("%s Valid until %v", tls, domain.Validity))
 		}
 		t.Append(row)
 	}
