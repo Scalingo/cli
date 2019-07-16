2f2fea5e8697dd1d6af49f24f730a187a0540c53
Jonathan Hurter <john@scalingo.com>
2019-07-16 14:54:44 +0200

    Add migration list

--- cmd/region_migrations.go
+++ cmd/region_migrations.go
@@ -12,13 +12,12 @@ var (
 		Category: "Region migrations",
 		Flags: []cli.Flag{
 			appFlag,
-			cli.BoolFlag{Name: "follow, f", Usage: "Follow the deployment progress"},
 			cli.StringFlag{Name: "to", Usage: "Select the destination region"},
 		},
 		Usage: "Migrate an app to another region",
 		Description: `Migrate an app to another region.
 	 Example
-	   'scalingo --app my-app migrations-create --to osc-fr1 --follow'
+	   'scalingo --app my-app migrations-create --to osc-fr1'
 		`,
 		Action: func(c *cli.Context) {
 			currentApp := appdetect.CurrentApp(c)
@@ -38,4 +37,28 @@ var (
 			}
 		},
 	}
+	migrationListCommand = cli.Command{
+		Name:     "migrations",
+		Category: "Region migrations",
+		Flags: []cli.Flag{
+			appFlag,
+		},
+		Usage: "List all migrations linked to an app",
+		Description: `List all migrations linked to an app
+   Example
+	   'scalingo --app my-app migrations'
+		`,
+		Action: func(c *cli.Context) {
+			currentApp := appdetect.CurrentApp(c)
+
+			err := region_migrations.List(currentApp)
+			if err != nil {
+				errorQuit(err)
+			}
+		},
+	}
 )
+
+//scalingo migrations-create -a app --to osc-fr1 --follow
+//scalingo migrations-follow migration-id
+//scalingo migrations
--- region_migrations/list.go
+++ region_migrations/list.go
@@ -1,1 +1,42 @@
 package region_migrations
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Scalingo/cli/config"
+	"github.com/olekukonko/tablewriter"
+	errgo "gopkg.in/errgo.v1"
+)
+
+func List(appId string) error {
+	c, err := config.ScalingoClient()
+	if err != nil {
+		return errgo.Notef(err, "fail to get scalingo client")
+	}
+
+	migrations, err := c.ListRegionMigrations(appId)
+	if err != nil {
+		return errgo.Notef(err, "fail to list migrations")
+	}
+	if len(migrations) == 0 {
+		fmt.Println("No migrations found for this app")
+		return nil
+	}
+
+	t := tablewriter.NewWriter(os.Stdout)
+	t.SetHeader([]string{"ID", "Destination", "Started At", "Finished At", "Status"})
+
+	for _, migration := range migrations {
+		t.Append([]string{
+			migration.ID,
+			migration.Destination,
+			migration.StartedAt.String(),
+			migration.FinishedAt.String(),
+			formatMigrationStatus(migration.Status),
+		})
+	}
+	t.Render()
+
+	return nil
+}
