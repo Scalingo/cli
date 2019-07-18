c4fbf42c82e21869490fbc201e286280acd9ced8
Étienne Michon <etienne@scalingo.com>
2019-07-18 16:36:54 +0200

    Follow backup creation

--- db/backup_create.go
+++ db/backup_create.go
@@ -1,21 +1,54 @@
 package db
 
 import (
+	"time"
+
 	"github.com/Scalingo/cli/config"
 	"github.com/Scalingo/cli/io"
+	scalingo "github.com/Scalingo/go-scalingo"
+	"github.com/briandowns/spinner"
+	"github.com/fatih/color"
 	errgo "gopkg.in/errgo.v1"
 )
 
 func CreateBackup(app, addon string) error {
+	spinner := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
+	spinner.Suffix = " Schedule a new backup"
+	spinner.Start()
+	defer spinner.Stop()
+
 	client, err := config.ScalingoClient()
 	if err != nil {
 		return errgo.Notef(err, "fail to get Scalingo client")
 	}
-	err = client.BackupCreate(app, addon)
+	backup, err := client.BackupCreate(app, addon)
 	if err != nil {
 		return err
 	}
 
-	io.Status("Successfully ordered to make a backup")
+	for backup.Status != scalingo.BackupStatusDone &&
+		backup.Status != scalingo.BackupStatusError {
+		spinner.Lock()
+		if backup.Status == scalingo.BackupStatusScheduled {
+			spinner.Suffix = " Waiting for the backup to start"
+		} else {
+			spinner.Suffix = " Waiting for the backup to finish"
+		}
+		spinner.Unlock()
+
+		time.Sleep(1 * time.Second)
+
+		backup, err = client.BackupShow(app, addon, backup.ID)
+		if err != nil {
+			return errgo.Notef(err, "fail to refresh backup state")
+		}
+	}
+	spinner.Stop()
+
+	if backup.Status == scalingo.BackupStatusDone {
+		io.Status(color.New(color.FgGreen).Sprint("Backup successfuly finished"))
+	} else {
+		io.Error(color.New(color.FgRed).Sprintf("Backup failed"))
+	}
 	return nil
 }
