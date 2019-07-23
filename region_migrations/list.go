fd7546a8cfc96bf7638e0c7d85c5e31cb27005de
Jonathan Hurter <john@scalingo.com>
2019-07-23 14:24:14 +0200

    Prettier list output

--- region_migrations/list.go
+++ region_migrations/list.go
@@ -3,8 +3,11 @@ package region_migrations
 import (
 	"fmt"
 	"os"
+	"sort"
+	"time"
 
 	"github.com/Scalingo/cli/config"
+	"github.com/fatih/color"
 	"github.com/olekukonko/tablewriter"
 	errgo "gopkg.in/errgo.v1"
 )
@@ -24,15 +27,24 @@ func List(appId string) error {
 		return nil
 	}
 
+	sort.Slice(migrations, func(i, j int) bool {
+		return migrations[i].StartedAt.Unix() > migrations[j].StartedAt.Unix()
+	})
+
 	t := tablewriter.NewWriter(os.Stdout)
 	t.SetHeader([]string{"ID", "Destination", "Started At", "Finished At", "Status"})
 
 	for _, migration := range migrations {
+		finishedAt := migration.FinishedAt.Local().Format(time.RFC822)
+		if migration.FinishedAt.IsZero() {
+			finishedAt = color.BlueString("Ongoing")
+		}
+
 		t.Append([]string{
 			migration.ID,
 			migration.Destination,
-			migration.StartedAt.String(),
-			migration.FinishedAt.String(),
+			migration.StartedAt.Local().Format(time.RFC822),
+			finishedAt,
 			formatMigrationStatus(migration.Status),
 		})
 	}
