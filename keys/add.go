1ccd6f6b98b6d2dfdfbbc5f10bff634bc7d85317
Étienne Michon <etienne@scalingo.com>
2019-08-29 09:48:26 +0200

    Fix the tests

--- keys/add.go
+++ keys/add.go
@@ -12,18 +12,18 @@ import (
 func Add(name string, path string) error {
 	stat, err := os.Stat(path)
 	if err != nil {
-		return errgo.Mask(err)
+		return errgo.Notef(err, "fail to stat path to the key")
 	}
 	if stat.Mode().IsDir() {
-		return errgo.Newf("%s: is a directory", path)
+		return fmt.Errorf("%s: is a directory", path)
 	}
 	if stat.Size() > 10*1024*1024 {
-		return errgo.Newf("%s: is too large (%v bytes)", stat.Size())
+		return fmt.Errorf("%s: is too large (%v bytes)", path, stat.Size())
 	}
 
 	keyContent, err := ioutil.ReadFile(path)
 	if err != nil {
-		return errgo.Mask(err)
+		return errgo.Notef(err, "fail to read the key file")
 	}
 
 	c, err := config.ScalingoClient()
@@ -32,7 +32,7 @@ func Add(name string, path string) error {
 	}
 	_, err = c.KeysAdd(name, string(keyContent))
 	if err != nil {
-		return errgo.Mask(err)
+		return errgo.Notef(err, "fail to add the key to Scalingo account")
 	}
 
 	fmt.Printf("Key '%s' has been added.\n", name)
