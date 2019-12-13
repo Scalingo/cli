7d76d5e2ae1342671ade6ad3f14845358f6f383c
Brandon WELSCH <dev@brandon-welsch.eu>
2019-12-13 15:36:21 +0100

    Add more test with many different type of keys

--- crypto/sshkeys/private_key_test.go
+++ crypto/sshkeys/private_key_test.go
@@ -29,8 +29,53 @@ func TestPrivateKey_IsEncrypted(t *testing.T) {
 			ExpectBoolean: true,
 		},
 		{
+			Name:          "Encrypted AES RSA Key (Mac OS Maverick)",
+			PrivateKey:    &PrivateKey{Path: "n/a", Block: pemDecode(aesRSAMacOSMaverick)},
+			ExpectBoolean: true,
+		},
+		{
+			Name:          "Encrypted AES RSA Key (Mac OS Yosemite)",
+			PrivateKey:    &PrivateKey{Path: "n/a", Block: pemDecode(aesRSAMacOSYosemite)},
+			ExpectBoolean: true,
+		},
+		{
 			Name:          "Encrypted DES3 RSA Key",
 			PrivateKey:    &PrivateKey{Path: "n/a", Block: pemDecode(des3RSA)},
+			ExpectBoolean: true,
+		},
+		{
+			Name:          "Encrypted DES3 RSA Key with Long Passphrase",
+			PrivateKey:    &PrivateKey{Path: "n/a", Block: pemDecode(des3RSALongPassphrase)},
+			ExpectBoolean: true,
+		},
+		{
+			Name:          "Unencrypted OpenSSH RSA Key",
+			PrivateKey:    &PrivateKey{Path: "n/a", Block: pemDecode(unencryptedOpenSSHRSA)},
+			ExpectBoolean: false,
+		},
+		{
+			Name:          "Encrypted OpenSSH RSA Key",
+			PrivateKey:    &PrivateKey{Path: "n/a", Block: pemDecode(openSSHRSA)},
+			ExpectBoolean: true,
+		},
+		{
+			Name:          "Unencrypted OpenSSH ecdsa Key",
+			PrivateKey:    &PrivateKey{Path: "n/a", Block: pemDecode(unencryptedOpenSSHecdsa)},
+			ExpectBoolean: false,
+		},
+		{
+			Name:          "Encrypted OpenSSH ecdsa Key",
+			PrivateKey:    &PrivateKey{Path: "n/a", Block: pemDecode(openSSHecdsa)},
+			ExpectBoolean: true,
+		},
+		{
+			Name:          "Unencrypted OpenSSH ed25519 Key",
+			PrivateKey:    &PrivateKey{Path: "n/a", Block: pemDecode(unencryptedOpenSSHed25519)},
+			ExpectBoolean: false,
+		},
+		{
+			Name:          "Encrypted OpenSSH ed25519 Key",
+			PrivateKey:    &PrivateKey{Path: "n/a", Block: pemDecode(openSSHed25519)},
 			ExpectBoolean: true,
 		},
 	}
