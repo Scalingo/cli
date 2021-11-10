9b5014e2371401b19b54b72c66855216ec7117b6
Étienne Michon <etienne@scalingo.com>
2021-11-10 15:08:38 +0100

    fix(term): prefer the use of the os package rather than syscall
    The syscall package documentation states:
    
    > Package syscall contains an interface to the low-level operating system primitives. The details vary depending on the underlying system. [...] The primary use of syscall is inside other packages that provide a more portable interface to the system, such as "os", "time" and "net". Use those packages rather than this one if you can.
    
    https://pkg.go.dev/syscall
    
    With this PR, the CLI compiles again on all OS

--- term/password.go
+++ term/password.go
@@ -2,7 +2,7 @@ package term
 
 import (
 	"fmt"
-	"syscall"
+	"os"
 
 	"golang.org/x/term"
 	"gopkg.in/errgo.v1"
@@ -10,7 +10,7 @@ import (
 
 func Password(prompt string) (string, error) {
 	fmt.Print(prompt)
-	bytePassword, err := term.ReadPassword(syscall.Stdin)
+	bytePassword, err := term.ReadPassword(int(os.Stdin.Fd()))
 	if err != nil {
 		return "", errgo.Notef(err, "fail to read the password on stdin")
 	} else {
