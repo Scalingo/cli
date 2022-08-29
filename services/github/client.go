2e09205b0de073cae8d7f79365ee9a87ead6415e
Étienne Michon <etienne@scalingo.com>
2022-08-29 16:53:35 +0200

    refactor(services): constructor returns an interface

--- services/github/client.go
+++ services/github/client.go
@@ -13,25 +13,23 @@ const (
 	repo  = "cli"
 )
 
-type Client struct {
+type Client interface {
+	GetLatestRelease(ctx context.Context) (*github.RepositoryRelease, error)
+}
+
+type client struct {
 	githubRepoService *github.RepositoriesService
-}
-
-func (c Client) ListReleases(ctx context.Context, opts *github.ListOptions) ([]*github.RepositoryRelease, error) {
-	repoReleases, githubResponse, err := c.githubRepoService.ListReleases(ctx, owner, repo, opts)
-	debug.Printf("GitHub response: %#v\n", githubResponse)
-	return repoReleases, err
-}
-
-func (c Client) GetLatestRelease(ctx context.Context) (*github.RepositoryRelease, error) {
-	repoRelease, githubResponse, err := c.githubRepoService.GetLatestRelease(ctx, owner, repo)
-	debug.Printf("github response: %#v\n", githubResponse)
-	return repoRelease, err
 }
 
 func NewClient() Client {
 	githubClient := github.NewClient(nil)
-	return Client{
+	return client{
 		githubRepoService: githubClient.Repositories,
 	}
+}
+
+func (c client) GetLatestRelease(ctx context.Context) (*github.RepositoryRelease, error) {
+	repoRelease, githubResponse, err := c.githubRepoService.GetLatestRelease(ctx, owner, repo)
+	debug.Printf("github response: %#v\n", githubResponse)
+	return repoRelease, err
 }
