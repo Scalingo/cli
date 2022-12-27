a0eaa7c08fb34f30d3320426b1c70abd1096a039
Quentin Escudier <quentin.escudier@scalingo.com>
2022-12-27 15:37:42 +0100

    chore(send-signal): sending container id instead of the container name

--- apps/sendsignal.go
+++ apps/sendsignal.go
@@ -7,9 +7,10 @@ import (
 	"gopkg.in/errgo.v1"
 
 	"github.com/Scalingo/cli/config"
+	"github.com/Scalingo/go-scalingo/v6"
 )
 
-func SendSignal(ctx context.Context, app string, signal string, containerNames []string) error {
+func SendSignal(ctx context.Context, appName string, signal string, containerNames []string) error {
 	if len(containerNames) == 0 {
 		return errgo.New("at least one container name should be given")
 	}
@@ -18,8 +19,24 @@ func SendSignal(ctx context.Context, app string, signal string, containerNames [
 		return errgo.Notef(err, "fail to get Scalingo client to send signal to application containers")
 	}
 
+	containers, err := c.AppsContainersPs(ctx, appName)
+	if err != nil {
+		return errgo.Notef(err, "fail to list the application containers to get the ID of the container to stop")
+	}
+
 	for _, containerName := range containerNames {
-		err := c.ContainersSendSignal(ctx, app, signal, containerName)
+		var containerToStop *scalingo.Container
+		for _, container := range containers {
+			if container.Label == containerName {
+				containerToStop = &container
+				break
+			}
+		}
+		if containerToStop == nil {
+			return fmt.Errorf("The container '%s' does not exist", containerName)
+		}
+
+		err := c.ContainersSendSignal(ctx, appName, signal, containerToStop.ID)
 		if err != nil {
 			return errgo.Notef(err, "fail to send signal to container")
 		}
