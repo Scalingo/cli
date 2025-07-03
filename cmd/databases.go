e9750c6cdade24d7cf520003a09d17ff43dd91ac
Matthieu Bronner <matthieu.bronner@scalingo.com>
2025-07-03 15:38:34 +0200

    Add confirmation before unscheduling a periodic backup

--- cmd/databases.go
+++ cmd/databases.go
@@ -115,6 +115,12 @@ var (
 			}
 
 			if disable {
+				continueB := askContinue("This operation will disable the periodic backups of a database. Are you sure?")
+				if !continueB {
+					errorQuit(c.Context, errors.New("unschedule operation was not confirmed"))
+					return nil
+				}
+
 				params.Enabled = utils.BoolPtr(false)
 			}
 			if scheduleAtFlag != "" {
