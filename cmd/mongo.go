c5be8451c3004d5635ffdc5826ca939db6400346
Matthieu Bronner <matthieu.bronner@scalingo.com>
2025-08-13 14:33:54 +0200

    Move mongo

--- cmd/mongo.go
+++ cmd/mongo.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"context"
+
 	"github.com/urfave/cli/v3"
 
 	"github.com/Scalingo/cli/cmd/autocomplete"
@@ -39,15 +41,15 @@ http://doc.scalingo.com/internals/container-sizes.html`,
 			}
 
 			currentApp := detect.CurrentApp(c)
-			utils.CheckForConsent(c.Context, currentApp, utils.ConsentTypeDBs)
+			utils.CheckForConsent(ctx, currentApp, utils.ConsentTypeDBs)
 
-			err := db.MongoConsole(c.Context, db.MongoConsoleOpts{
+			err := db.MongoConsole(ctx, db.MongoConsoleOpts{
 				App:          currentApp,
 				Size:         c.String("s"),
 				VariableName: c.String("e"),
 			})
 			if err != nil {
-				errorQuit(c.Context, err)
+				errorQuit(ctx, err)
 			}
 			return nil
 		},
