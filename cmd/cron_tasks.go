09c10b1c13a0335b1e83148193f79329f04acbc9
Matthieu Bronner <matthieu.bronner@scalingo.com>
2025-08-13 14:00:32 +0200

    Move cron tasks

--- cmd/cron_tasks.go
+++ cmd/cron_tasks.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"context"
+
 	"github.com/urfave/cli/v3"
 
 	"github.com/Scalingo/cli/cmd/autocomplete"
@@ -21,19 +23,19 @@ var (
 
 		Action: func(ctx context.Context, c *cli.Command) error {
 			if c.Args().Len() > 0 {
-				cli.ShowCommandHelp(ctx, c, "cron-tasks")
+				_ = cli.ShowCommandHelp(ctx, c, "cron-tasks")
 				return nil
 			}
 
 			currentApp := detect.CurrentApp(c)
-			err := crontasks.List(c.Context, currentApp)
+			err := crontasks.List(ctx, currentApp)
 			if err != nil {
-				errorQuit(c.Context, err)
+				errorQuit(ctx, err)
 			}
 			return nil
 		},
 		ShellComplete: func(ctx context.Context, c *cli.Command) {
-			autocomplete.CmdFlagsAutoComplete(c, "cron-tasks")
+			_ = autocomplete.CmdFlagsAutoComplete(c, "cron-tasks")
 		},
 	}
 )
