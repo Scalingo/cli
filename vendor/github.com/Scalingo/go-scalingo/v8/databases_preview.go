bffe28678608591e7a82d20f34c557da5fcae3d7
Baptiste MAURY <baptiste.maury@scalingo.com>
2025-12-02 18:10:06 +0100

    Implement new local version of go-scalingo

--- vendor/github.com/Scalingo/go-scalingo/v8/databases_preview.go
+++ vendor/github.com/Scalingo/go-scalingo/v8/databases_preview.go
@@ -27,23 +27,17 @@ var _ DatabasesPreviewService = (*PreviewClient)(nil)
 
 // DatabaseNG stands for Database Next Generation.
 type DatabaseNG struct {
-	DatabaseInfo
-	Database *Database `json:"database,omitempty"`
-	App      App       `json:"app"`
+	ID         string    `json:"id"`
+	Name       string    `json:"name"`
+	ProjectID  string    `json:"project_id"`
+	Technology string    `json:"technology"`
+	Plan       string    `json:"plan"`
+	Database   *Database `json:"database,omitempty"`
+	App        App       `json:"app"`
 }
 
-type databaseNgResponse struct {
-	// App      App          `json:"app"` 	// Thoses fields will be removed
-	// Addon    Addon        `json:"addon"` // Thoses fields will be removed
-	Database DatabaseInfo `json:"database"`
-}
-
-type DatabaseInfo struct {
-	ID         string `json:"id"`
-	Name       string `json:"name"`
-	ProjectID  string `json:"project_id"`
-	Technology string `json:"technology"`
-	Plan       string `json:"plan"`
+type databaseApiResponse struct {
+	Database DatabaseNG `json:"database"`
 }
 
 type PreviewClient struct {
@@ -75,15 +69,15 @@ func (c *PreviewClient) DatabaseCreate(ctx context.Context, params DatabaseCreat
 
 func (c *PreviewClient) DatabasesList(ctx context.Context) ([]DatabaseNG, error) {
 	var res []DatabaseNG
-	var listResp []databaseNgResponse
+	var listResp []databaseApiResponse
 
 	err := c.parent.ScalingoAPI().ResourceList(ctx, databasesResource, nil, &listResp)
 	if err != nil {
 		return res, errors.Wrap(ctx, err, "list databases")
 	}
 
-	for _, response := range listResp {
-		databaseNG, err := c.formatDatabaseNG(ctx, response)
+	for _, apiResponse := range listResp {
+		databaseNG, err := c.populateApiResponseWithAppAndAddon(ctx, apiResponse)
 		if err != nil {
 			return res, errors.Wrap(ctx, err, "populate databaseNG")
 		}
@@ -137,26 +131,24 @@ func (c *PreviewClient) searchDatabase(ctx context.Context, appID string) (Datab
 	return res, ErrDatabaseNotFound
 }
 
-// formatDatabaseNG populates a DatabaseNG without using the App and Addon from the databases endpoints.
-func (c *PreviewClient) formatDatabaseNG(ctx context.Context, response databaseNgResponse) (DatabaseNG, error) {
-	var res DatabaseNG
+// populateApiResponseWithAppAndAddon populates a DatabaseNG without using the App and Addon from the databases endpoints.
+func (c *PreviewClient) populateApiResponseWithAppAndAddon(ctx context.Context, apiResponse databaseApiResponse) (DatabaseNG, error) {
+	var res = apiResponse.Database
 
-	res.DatabaseInfo = response.Database
-
-	addons, err := c.parent.AddonsList(ctx, response.Database.ID)
+	addons, err := c.parent.AddonsList(ctx, apiResponse.Database.ID)
 	if err != nil {
 		return res, errors.Wrap(ctx, err, "list addons")
 	}
 
-	appPtr, err := c.parent.AppsShow(ctx, response.Database.Name)
+	appPtr, err := c.parent.AppsShow(ctx, apiResponse.Database.Name)
 	if err != nil {
 		return res, errors.Wrap(ctx, err, "show app")
 	}
 	res.App = *appPtr
 
-	database, err := c.parent.DatabaseShow(ctx, response.Database.ID, addons[0].ID)
+	database, err := c.parent.DatabaseShow(ctx, apiResponse.Database.ID, addons[0].ID)
 	if err != nil {
-		fmt.Printf("Addons probably deleted for app: %+v\n", res.DatabaseInfo.Name)
+		fmt.Printf("Addons probably deleted for app: %+v\n", res.Name)
 	}
 	res.Database = &database
 
