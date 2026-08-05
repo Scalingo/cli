84883f2ab5ae08e5a2a3875ab7aa352b9d3f7db7
Étienne Michon <etienne@scalingo.com>
2026-08-05 17:14:03 +0200

    feat(cmd): add `valkey-console`

---
+++ cmd/valkey.go
@@ -0,0 +1,67 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/urfave/cli/v3"
+
+	"github.com/Scalingo/cli/cmd/autocomplete"
+	"github.com/Scalingo/cli/db"
+	"github.com/Scalingo/cli/detect"
+	"github.com/Scalingo/cli/utils"
+)
+
+var (
+	valkeyConsoleCommand = cli.Command{
+		Name:     "valkey-console",
+		Category: "Databases",
+		Usage:    "Run an interactive console with your Valkey addon",
+		Flags: []cli.Flag{&appFlag,
+			&cli.StringFlag{
+				Name: "size", Aliases: []string{"s"}, Value: "",
+				Usage: "Size of the container",
+			},
+			&cli.StringFlag{
+				Name: "env", Aliases: []string{"e"}, Value: "",
+				Usage: "Environment variable name to use for the connection to the database",
+			},
+		},
+		Description: CommandDescription{
+			Description: `Run an interactive console with your Valkey addon.
+
+The --size flag makes it easy to specify the size of the container executing
+the Valkey console. Each container size has different price and performance.
+You can read more about container sizes here:
+http://doc.scalingo.com/internals/container-sizes.html`,
+			Examples: []string{
+				"scalingo --app my-app valkey-console",
+				"scalingo --app my-app valkey-console --size L",
+				"scalingo --app my-app valkey-console --env MY_VALKEY_URL",
+			},
+			SeeAlso: []string{"mysql-console", "pgsql-console"},
+		}.Render(),
+
+		Action: func(ctx context.Context, c *cli.Command) error {
+			if c.Args().Len() != 0 {
+				_ = cli.ShowCommandHelp(ctx, c, "valkey-console")
+				return nil
+			}
+			currentApp := detect.CurrentApp(ctx, c)
+
+			utils.CheckForConsent(ctx, currentApp, utils.ConsentTypeDBs)
+
+			err := db.ValkeyConsole(ctx, db.ValkeyConsoleOpts{
+				App:          currentApp,
+				Size:         c.String("s"),
+				VariableName: c.String("e"),
+			})
+			if err != nil {
+				errorQuit(ctx, err)
+			}
+			return nil
+		},
+		ShellComplete: func(_ context.Context, c *cli.Command) {
+			_ = autocomplete.CmdFlagsAutoComplete(c, "valkey-console")
+		},
+	}
+)
---
+++ db/valkey_console.go
@@ -0,0 +1,80 @@
+package db
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"strings"
+
+	"github.com/Scalingo/cli/apps"
+	"github.com/Scalingo/go-utils/errors/v3"
+)
+
+type ValkeyConsoleOpts struct {
+	App          string
+	Size         string
+	VariableName string
+}
+
+func ValkeyConsole(ctx context.Context, opts ValkeyConsoleOpts) error {
+	if opts.VariableName == "" {
+		opts.VariableName = "SCALINGO_VALKEY"
+	}
+	valkeyURL, _, password, err := dbURL(ctx, opts.App, opts.VariableName, []string{"valkey", "valkeys"})
+	if err != nil {
+		return errors.Wrapf(ctx, err, "resolve Valkey URL from %s", opts.VariableName)
+	}
+
+	if valkeyURL.Scheme == "valkeys" {
+		return errors.New(ctx, "Valkey console is not available when TLS connections are enforced")
+	}
+
+	host, port, err := net.SplitHostPort(valkeyURL.Host)
+	if err != nil {
+		return fmt.Errorf("%v has an invalid host", valkeyURL)
+	}
+
+	err = apps.Run(ctx, apps.RunOpts{
+		DisplayCmd:    "valkey-console " + strings.Split(host, ".")[0],
+		App:           opts.App,
+		Cmd:           []string{"dbclient-fetcher", "valkey", "&&", "valkey-cli", "-h", host, "-p", port, "-a", password},
+		Size:          opts.Size,
+		StdinCopyFunc: valkeyStdinCopy,
+	})
+	if err != nil {
+		return fmt.Errorf("run Valkey console: %v", err)
+	}
+
+	return nil
+}
+
+func valkeyStdinCopy(dst io.Writer, src io.Reader) (int64, error) {
+	var written int64
+	buf := make([]byte, 2*1024)
+	for {
+		nr, er := src.Read(buf)
+		if nr > 0 {
+			toWrite := bytes.ReplaceAll(buf[0:nr], []byte{'\n'}, []byte{'\r', '\n'})
+			nr = len(toWrite)
+			nw, ew := dst.Write(toWrite)
+			if nw > 0 {
+				written += int64(nw)
+			}
+			if ew != nil {
+				return written, ew
+			}
+			if nr != nw {
+				return written, io.ErrShortWrite
+			}
+		}
+		if er == io.EOF {
+			break
+		}
+		if er != nil {
+			return written, er
+		}
+	}
+	return written, nil
+}
